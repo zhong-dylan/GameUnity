622881952f97ea2f8fa0d1fb0e9e230b2ec27a47
Dylan Zhong <dylan.zhong@corp.netease.com>
2022-11-09 16:39:01 +0800

    LinkGame

---
+++ LinkGame/Unity/Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance
+    {
+        get { return this; }
+    }
+}
---
+++ LinkGame/Unity/Assets/Game/Scripts/UIManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public UIManager instance
+    {
+        get { return this; }
+    }
+}
